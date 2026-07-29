7fe68514e2fa9d3f7f8418cb26b6b0ae2bc99ee4
Nazarch1koff <tarakanovnazar08@gmail.com>
2026-07-29 15:32:02 +0300

    Update auth.js

---
+++ js/auth.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const firebaseConfig = {
+
+    apiKey: "ТВОЙ_APIKEY",
+    authDomain: "ТВОЙ_AUTHDOMAIN",
+    projectId: "ТВОЙ_PROJECTID",
+    storageBucket: "ТВОЙ_STORAGE",
+    messagingSenderId: "ТВОЙ_SENDER",
+    appId: "ТВОЙ_APPID"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+document
+.getElementById("googleLogin")
+.addEventListener("click", async ()=>{
+
+    try{
+
+        const result = await signInWithPopup(auth,provider);
+
+        alert("Добро пожаловать, " + result.user.displayName);
+
+        console.log(result.user);
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        alert("Ошибка входа");
+
+    }
+
+});

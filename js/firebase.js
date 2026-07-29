8510b8540ff03f5d90815b4df3d97d03f7cd0f0c
Nazarch1koff <tarakanovnazar08@gmail.com>
2026-07-29 15:21:12 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
 
+const firebaseConfig = {
+
+    // сюда вставишь свой firebaseConfig
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
